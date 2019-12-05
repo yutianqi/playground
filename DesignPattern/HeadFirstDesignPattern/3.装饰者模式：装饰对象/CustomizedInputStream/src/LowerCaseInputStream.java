a588e077956f1ab1abbcd1729e1f3f78f2051b6a
y00290641 <yutianqi@huawei.com>
2019-12-05 16:39:40 +0800

    Update

---
+++ DesignPattern/HeadFirstDesignPattern/3.装饰者模式：装饰对象/CustomizedInputStream/src/LowerCaseInputStream.java
@@ -0,0 +1,16 @@
+import java.io.FilterInputStream;
+
+/**
+ * 功能描述
+ *
+ * @author y00290641
+ * @version V300R005C10, 2019/12/5
+ * @since V300R005C10
+ */
+public class LowerCaseInputStream extends FilterInputStream {
+
+
+
+
+
+}
